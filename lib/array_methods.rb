1e8a5ff9bcc1fbc38da6f47481557b3104579dd7
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-04 21:27:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
-  array.length.times do |index|
-    if array[index] == value_to_find
-      puts array.at(value_to_find)
+  array.length.times do |value_to_find|
+    if array[value_to_find] == value_to_find
+      puts array[value_to_find]
     else
       puts
     end
